1627317378386712f5f9cbf12ac20b65dae21e7f
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-03-19 14:10:09 +0600

    comment done in code

--- js/event.js
+++ js/event.js
@@ -1,8 +1,14 @@
+//get coomment box post button by id
 document.getElementById('post').addEventListener('click', function () {
+    //get inner text value from comment box by id
     const commentBox = document.getElementById('comment-box');
     const comment = commentBox.value;
     // console.log(comment);
+    //get comment field to show the comment by id
     const commentField = document.getElementById('comment-display-field');
+    //set new comment to comment field
     commentField.innerText = comment;
+
+    //set comment box empty
     commentBox.value = ' ';
 })
